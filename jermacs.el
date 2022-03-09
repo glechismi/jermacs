1a0db8b264e0a3ca597d052a85b3726cb81d35c7
glechismi <94998063+glechismi@users.noreply.github.com>
2022-03-08 23:48:38 -0500

    Add files via upload

---
+++ jermacs.el
@@ -0,0 +1,32 @@
+;;; jermacs.el --- My own custom theme and some other little emacs stuff  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Jeremy Clark
+
+;; Author: Jeremy Clark <jermclk@guixPad>
+;; Keywords: internal
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(load-theme 'HexDark)
+(require 'mode-line-bell)
+(require 'page-break)
+(require 'keymaps)
+
+(provide 'jermacs)
+;;; jermacs.el ends here
